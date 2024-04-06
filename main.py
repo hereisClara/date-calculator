d991778f437776bd0357148e1813d1cda80b417f
Clara <165897678+hereisClara@users.noreply.github.com>
2024-04-06 18:17:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-# from datetime import datetime
-# user_date_str = input("請輸入日期（年/月/日）：")
-# user_date_name = input("請輸入紀念日名稱：")
-# user_date_story = input("在這一天發生了什麼事？")
-
-# user_date = datetime.strptime(user_date_str, "%Y/%m/%d")
-# today = datetime.today()
-# delta_date = abs(today - user_date).days
-
-# while not user_date_str.strip():
-#   print("請重新輸入日期")
-#   user_date_str = input("請輸入日期（年/月/日）：")
-
-# else:
-#   print(f'{user_date_name}已經過了{delta_date}天，在{user_date_str}這一天你{user_date_story}。')
-
 from datetime import datetime
 from colorama import Fore
 
